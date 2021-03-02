1703f7ce1862a8d7a4933933bd8e106a97f4d710
controlaltdelete123 <72532759+controlaltdelete123@users.noreply.github.com>
2021-03-02 09:29:46 +0800

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author Ariz De Jesus
+ */
+public class Array {
+
+ 
+    public static void main(String[] args) {
+        int a[] = new int[4];
+        a[1]=2;
+        a[2]=3;
+        System.out.println(a[1]);
+    }
+    
+}
